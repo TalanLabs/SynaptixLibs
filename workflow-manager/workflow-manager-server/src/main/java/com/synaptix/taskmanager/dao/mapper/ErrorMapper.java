df8765b6c654ed1838105baffa0f1999e79db127
Nicolas Sauvage <nicolas.sauvage@synaptix-labs.com>
2015-09-01 16:34:53 +0200

    update error mappers

--- workflow-manager/workflow-manager-server/src/main/java/com/synaptix/taskmanager/dao/mapper/ErrorMapper.java
+++ workflow-manager/workflow-manager-server/src/main/java/com/synaptix/taskmanager/dao/mapper/ErrorMapper.java
@@ -1,22 +1,13 @@
 package com.synaptix.taskmanager.dao.mapper;
 
 import java.io.Serializable;
-import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
-import com.synaptix.entity.IErrorEntity;
-
 public interface ErrorMapper {
 
-	public void deleteErrorsByIdObject(@Param("idObject") Serializable idObject);
+	void deleteErrorsByIdObject(@Param("idObject") Serializable idObject);
 
-	public void deleteErrorsByIdTask(@Param("idTask") Serializable idTask);
-
-	public void pingDB();
-
-	public List<IErrorEntity> searchErrorsByIdObject(@Param("idObject") Serializable idObject);
-
-	public IErrorEntity findById(@Param("id") Serializable idError);
+	void deleteErrorsByIdTask(@Param("idTask") Serializable idTask);
 
 }
