9df014f9cfa7d9adcd3a6eb1d357f9543b6654dc
Nicolas Sauvage <nicolas.sauvage@talanlabs.com>
2016-11-10 15:47:20 +0100

    Revert "Merge branch 'master' into 'master'"
    This reverts merge request !10

--- workflow-manager/workflow-manager-server/src/main/java/com/synaptix/taskmanager/service/TaskManagerServerService.java
+++ workflow-manager/workflow-manager-server/src/main/java/com/synaptix/taskmanager/service/TaskManagerServerService.java
@@ -186,20 +186,6 @@ public class TaskManagerServerService extends AbstractSimpleService implements I
 		}
 	}
 
-	/**
-	 * Check if cluster has finished creating its tasks
-	 */
-	private boolean hasCreatedTasks(IId idTaskCluster) {
-		try {
-			getDaoSession().begin();
-			return getTaskMapper().findTasksByCluster(idTaskCluster).size() > 0;
-		} catch (Exception e) {
-			throw new ServiceException(e);
-		} finally {
-			getDaoSession().end();
-		}
-	}
-
 	@Override
 	public IServiceResult<Set<IError>> startEngine(IId idTaskCluster) {
 		return _startEngine(idTaskCluster);
@@ -210,7 +196,7 @@ public class TaskManagerServerService extends AbstractSimpleService implements I
 		if (LOG.isDebugEnabled()) {
 			LOG.debug("TM - StartEngine for cluster " + idTaskCluster);
 		}
-		if (idTaskCluster == null || !hasCreatedTasks(idTaskCluster)) {
+		if (idTaskCluster == null) {
 			return restart();
 		}
 
@@ -268,14 +254,14 @@ public class TaskManagerServerService extends AbstractSimpleService implements I
 					}
 
 					for (IId idTask : tasksLists.getIdTasksToRemove()) {
-						for (Iterator<ITask> iterator = recycleList.iterator(); iterator.hasNext();) {
+						for (Iterator<ITask> iterator = recycleList.iterator(); iterator.hasNext(); ) {
 							ITask iTask = iterator.next();
 							if (idTask.equals(iTask.getId())) {
 								iterator.remove();
 								break;
 							}
 						}
-						for (Iterator<ITask> iterator = tasksQueue.iterator(); iterator.hasNext();) {
+						for (Iterator<ITask> iterator = tasksQueue.iterator(); iterator.hasNext(); ) {
 							ITask iTask = iterator.next();
 							if (idTask.equals(iTask.getId())) {
 								iterator.remove();
