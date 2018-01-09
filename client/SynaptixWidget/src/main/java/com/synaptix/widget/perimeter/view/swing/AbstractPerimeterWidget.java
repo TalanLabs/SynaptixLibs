002210ed3f6ee1cac2bd7d75deecd8ef7f4637c2
Sami Mbarki <sami.mbarki@talan.tn>
2018-01-09 12:54:53 +0100

    Add right double click to clean perimeter widget fields

--- client/SynaptixWidget/src/main/java/com/synaptix/widget/perimeter/view/swing/AbstractPerimeterWidget.java
+++ client/SynaptixWidget/src/main/java/com/synaptix/widget/perimeter/view/swing/AbstractPerimeterWidget.java
@@ -1,9 +1,13 @@
 package com.synaptix.widget.perimeter.view.swing;
 
-import javax.swing.JComponent;
+import javax.swing.*;
 
 import com.synaptix.client.view.IView;
 import com.synaptix.swing.WaitComponentFeedbackPanel;
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public abstract class AbstractPerimeterWidget extends WaitComponentFeedbackPanel implements IPerimeterWidget, IView {
 
@@ -34,4 +38,36 @@ public abstract class AbstractPerimeterWidget extends WaitComponentFeedbackPanel
 			l.valuesChanged(this);
 		}
 	}
+
+    @Override
+    public void addContent(Component content) {
+        if(content != null && JTextField.class.isAssignableFrom(content.getClass())){
+            content.addMouseListener(new FieldMouseAdapter());
+        } else if (content != null && JPanel.class.isAssignableFrom(content.getClass())){
+            JPanel panel = (JPanel) content;
+            for(Component component:panel.getComponents()){
+                if(component != null && JTextField.class.isAssignableFrom(component.getClass())){
+                    component.addMouseListener(new FieldMouseAdapter());
+                }
+            }
+        }
+        super.addContent(content);
+    }
+
+    public class FieldMouseAdapter extends MouseAdapter{
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            //Right Double click
+            if ((e.getClickCount() == 2) && (e.getButton() == MouseEvent.BUTTON3)) {
+                if ((e.getSource() instanceof JFormattedTextField) && (((JFormattedTextField) e.getSource()).isEnabled())) {
+                    ((JFormattedTextField) e.getSource()).setValue(null);
+                    fireValuesChanged();
+                } else if ((e.getSource() instanceof JTextField) && (((JTextField) e.getSource()).isEnabled())) {
+                    ((JTextField) e.getSource()).setText(null);
+                    fireValuesChanged();
+                }
+            }
+        }
+    }
+
 }
