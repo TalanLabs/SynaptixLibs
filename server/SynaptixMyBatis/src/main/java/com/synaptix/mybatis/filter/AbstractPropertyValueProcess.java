e7abcc58878c8e909ed51d985efd15b93594c073
Aymen Bouchekoua <aymen.bouchakoua@gefco.net>
2018-02-22 17:21:57 +0100

    fix bug : giving an entity as an initial filter to a suggest widget fails

--- server/SynaptixMyBatis/src/main/java/com/synaptix/mybatis/filter/AbstractPropertyValueProcess.java
+++ server/SynaptixMyBatis/src/main/java/com/synaptix/mybatis/filter/AbstractPropertyValueProcess.java
@@ -3,6 +3,7 @@ package com.synaptix.mybatis.filter;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.synaptix.entity.IEntity;
 import com.synaptix.service.filter.leaf.AbstractPropertyValue;
 
 public abstract class AbstractPropertyValueProcess<E extends AbstractPropertyValue<?>> extends AbstractPropertyProcess<E> {
@@ -10,7 +11,9 @@ public abstract class AbstractPropertyValueProcess<E extends AbstractPropertyVal
 	@Override
 	public Map<String, Object> buildValueFilterMap(IFilterContext context, E node) {
 		Map<String, Object> res = new HashMap<String, Object>();
-		res.put(context.getParameterFieldName(node, node.getPropertyName()).replaceAll(".id", ""), node.getValue());
+		String parameterFieldName = context.getParameterFieldName(node, node.getPropertyName());
+		res.put(IEntity.class.isAssignableFrom(node.getValue().getClass()) ? parameterFieldName.replaceAll(".id", "") :
+				parameterFieldName, node.getValue());
 		return res;
 	}
 }
